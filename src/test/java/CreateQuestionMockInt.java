38782114a54eef634e4916c2250445ed282da1d4
Mikel <Mikel@10.110.5.232>
2022-09-29 12:52:24 +0200

    Mockito import ondo 2

--- src/test/java/CreateQuestionMockInt.java
+++ src/test/java/CreateQuestionMockInt.java
@@ -125,6 +125,7 @@ public class CreateQuestionMockInt {
 				e.printStackTrace();
 			}
 		   }
+	@SuppressWarnings("unchecked")
 	@Test
 	public void test7() {
 		try {
@@ -138,30 +139,31 @@ public class CreateQuestionMockInt {
 			} catch (ParseException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
-			}	
-			
-			//configure Mock
-			Mockito.doReturn(oneDate).when(mockedEvent).getEventDate();
-			Mockito.when(dataAccess.createQuestion(Mockito.any(Event.class),Mockito.any(String.class), Mockito.any(Integer.class))).thenThrow(QuestionAlreadyExist.class);
-			
+			}		
+			try {
+				//configure Mock
+				Mockito.doReturn(oneDate).when(mockedEvent).getEventDate();
+				Mockito.when(dataAccess.createQuestion(Mockito.any(Event.class),Mockito.any(String.class), Mockito.any(Integer.class))).thenThrow(QuestionAlreadyExist.class);
 
-			//invoke System Under Test (sut) 
-			sut.createQuestion(mockedEvent, queryText, betMinimum);
-			
-			//if the program continues fail
-		    fail();
-		   } catch (QuestionAlreadyExist e) {
-			// TODO Auto-generated catch block
-			   
-			// if the program goes to this point OK
-			assertTrue(true);
-			} catch (EventFinished e) {
-				// if the program goes to this point fail
-			    fail();
+				//invoke System Under Test (sut) 
+				sut.createQuestion(mockedEvent, queryText, betMinimum);
+			} catch (QuestionAlreadyExist e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
-			}
-		   }
+				// if the program goes to this point OK
+				assertTrue(true);
+			} 
+			
+			//if the program continues fail
+			fail();
+		}catch (EventFinished e) {
+			// if the program goes to this point fail
+			fail();
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
 	
 	
 	
