f63c79b0b547653edf3243947c3c6e1f3740e1b6
Mikel <mikelmartiniglesias@gmail.com>
2022-10-07 19:36:39 +0200

    TODO kenduta

--- src/main/java/gui/SarreraOntziaGUI.java
+++ src/main/java/gui/SarreraOntziaGUI.java
@@ -143,7 +143,6 @@ public class SarreraOntziaGUI extends JFrame implements ListCellRenderer{
 	@Override
 	public Component getListCellRendererComponent(JList list, Object value, int index, boolean isSelected,
 			boolean cellHasFocus) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 }
