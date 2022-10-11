5a59b75db13e63e70efa53fdca8e322f28ae8236
mikel0912 <mikelmartiniglesias@gmail.com>
2022-10-11 19:18:42 +0200

    Errefaktorizazioa code errepikapena mikel

--- src/main/java/dataAccess/DataAccess.java
+++ src/main/java/dataAccess/DataAccess.java
@@ -432,17 +432,18 @@ public class DataAccess implements DataAccessInterface {
 			reg2.addApustuAnitza(apA9);
 			ke8.eguneratuKont(1);
 			
-			Transaction t1 = new Transaction(reg1, apA1.getBalioa(), new Date(), "ApustuaEgin");
-			Transaction t3 = new Transaction(reg2, apA4.getBalioa(), new Date(), "ApustuaEgin");
-			Transaction t4 = new Transaction(reg3, apA5.getBalioa(), new Date(), "ApustuaEgin");
-			Transaction t5 = new Transaction(reg4, apA3.getBalioa(), new Date(), "ApustuaEgin");
-			Transaction t6 = new Transaction(reg4, apA6.getBalioa(), new Date(), "ApustuaEgin");
-			Transaction t7 = new Transaction(reg1, apA7.getBalioa(), new Date(), "ApustuaEgin");
-			Transaction t8 = new Transaction(reg1, apA8.getBalioa(), new Date(), "ApustuaEgin");
-			Transaction t9 = new Transaction(reg2, apA9.getBalioa(), new Date(), "ApustuaEgin");
-			Transaction t10 = new Transaction(reg2, apA10.getBalioa(), new Date(), "ApustuaEgin");
-			Transaction t11 = new Transaction(reg3, apA11.getBalioa(), new Date(), "ApustuaEgin");
-			Transaction t12 = new Transaction(reg3, apA12.getBalioa(), new Date(), "ApustuaEgin");
+			String apustuEginHitza = "ApustuaEgin";
+			Transaction t1 = new Transaction(reg1, apA1.getBalioa(), new Date(), apustuEginHitza);
+			Transaction t3 = new Transaction(reg2, apA4.getBalioa(), new Date(), apustuEginHitza);
+			Transaction t4 = new Transaction(reg3, apA5.getBalioa(), new Date(), apustuEginHitza);
+			Transaction t5 = new Transaction(reg4, apA3.getBalioa(), new Date(), apustuEginHitza);
+			Transaction t6 = new Transaction(reg4, apA6.getBalioa(), new Date(), apustuEginHitza);
+			Transaction t7 = new Transaction(reg1, apA7.getBalioa(), new Date(), apustuEginHitza);
+			Transaction t8 = new Transaction(reg1, apA8.getBalioa(), new Date(), apustuEginHitza);
+			Transaction t9 = new Transaction(reg2, apA9.getBalioa(), new Date(), apustuEginHitza);
+			Transaction t10 = new Transaction(reg2, apA10.getBalioa(), new Date(), apustuEginHitza);
+			Transaction t11 = new Transaction(reg3, apA11.getBalioa(), new Date(), apustuEginHitza);
+			Transaction t12 = new Transaction(reg3, apA12.getBalioa(), new Date(), apustuEginHitza);
 			
 			reg1.addTransaction(t1);
 			reg2.addTransaction(t3);
